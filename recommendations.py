0c3056d7023cc4b19c97a8c20838d4345da1409c
aditya <aditya8469@gmail.com>
2023-08-27 13:38:23 +0530

    funnctional improvements

--- recommendations.py
+++ recommendations.py
@@ -4,7 +4,6 @@ from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 
 def content_based_recommendation(input_features, df, top_n=5):
-    # Ensure input_features is a 2D array with shape (1, num_features)
     input_features = np.array(input_features).reshape(1, -1)
 
     # Extract audio features of all songs from the DataFrame
@@ -15,8 +14,6 @@ def content_based_recommendation(input_features, df, top_n=5):
 
     # Get indices of songs in descending order of similarity scores
     similar_songs_indices = np.argsort(similarity_scores[0])[::-1]
-
-    # Remove the input song itself from the list of similar songs
     similar_songs_indices = similar_songs_indices[1:]
 
     # Select top N songs
